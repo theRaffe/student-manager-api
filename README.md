20d1be222dc109e3c179aaa95f991927cb90d226
raffe <rafe2004@gmail.com>
2024-04-05 17:53:14 -0600

    * enable CORS * update README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+## Start dev server
+uvicorn main:app --reload --host 0.0.0.0
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 
 from fastapi import FastAPI
 from routers import student_router, teacher_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    expose_headers=["*"],
+    # https://stackoverflow.com/questions/65635346/how-can-i-enable-cors-in-fastapi
+    allow_methods=['*'], allow_headers=['*']
+
+)
+
 app.include_router(student_router.router)
 app.include_router(teacher_router.router)
 
